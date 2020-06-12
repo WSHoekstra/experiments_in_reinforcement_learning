e49eb0ac253148eaf5a35baf9dc86cd1d448ba08
WS Hoekstra <w.s.hoekstra@outlook.com>
2020-06-12 18:23:44 +0200

    tweak LR, save model after training

--- experiments/01_cartpole/cartpole.py
+++ experiments/01_cartpole/cartpole.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+import os
+# os.chdir('C:/Users/Walter/Documents/GitHub/experiments_in_reinforcement_learning')
+
 import gym
 import numpy as np
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 import random
 import collections
 
@@ -15,11 +20,10 @@ class Agent:
     def make_model(self, activation_function='tanh'):
         model = Sequential()
         model.add(Dense(units=self.observation_space_size, input_dim=self.observation_space_size, name='input')) # state + actions
-        model.add(Flatten()) # input the observations
-        model.add(Dense(units= 16, activation=activation_function)) # hidden
-        model.add(Dense(units= 8, activation=activation_function)) # hidden
+        model.add(Dense(units= (2 * self.observation_space_size), activation=activation_function)) # hidden
+        model.add(Dense(units=self.observation_space_size, activation=activation_function)) # hidden
         model.add(Dense(units=self.action_space_size, activation='linear', name='output')) # output the actions
-        model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate), metrics=['mae'])
         return model
     
     def commit_to_memory(self, state, action, reward, next_state, done):
@@ -62,16 +66,23 @@ class Agent:
             self._model = self.make_model()
         return self._model
     
+
+    def save_model_to_disk(self):
+        self.model.save(self.model_filepath)
+        
+    
     def __init__(self, 
                  observation_space_size, 
                  action_space_size, 
                  epsilon=0.999,
-                 epsilon_decay=0.99,
+                 epsilon_decay=0.95,
                  gamma=0.8,
                  epsilon_min=0,
                  memory_size=1024, 
-                 training_data_size=128,
-                 batch_size=32):
+                 training_data_size=8192,
+                 batch_size=32,
+                 learning_rate=0.001,
+                 load_model_from_disk=True):
         self._model = None
         self.observation_space_size = observation_space_size
         self.action_space_size = action_space_size
@@ -84,13 +95,20 @@ class Agent:
         self.epsilon_min = epsilon_min
         self.epsilon_decay = epsilon_decay
         self.gamma = gamma
+        self.learning_rate = learning_rate
+        self.model_filepath = '01_cartpolt/model.h5'
+        if load_model_from_disk:
+            try:
+                self._model = load_model(self.model_filepath)
+            except:
+                pass
+            
 
 agent = Agent(observation_space_size=env.observation_space.shape[0],
               action_space_size=env.action_space.n)
 
 # agent.model.summary()
 # out = agent.model.predict( [ [0.25, 0.5, 0.75, 1, 1] ] ) # untrained network output on a reset env
-
 
 done = False
 max_n_steps = 2000
@@ -124,4 +142,6 @@ for episode in episodes:
         state = next_state
     if episode % train_every_n_episodes == 0:
             agent.learn_from_memories()
+    agent.save_model_to_disk()
 env.close()
+
